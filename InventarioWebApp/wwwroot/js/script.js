dfa313cb8732b378e86cb0a585829cfbe5dc27e8
arin8191@gmail.com <Ariana Nez>
2023-04-06 13:08:43 -0500

    nav movido

--- InventarioWebApp/wwwroot/js/script.js
+++ InventarioWebApp/wwwroot/js/script.js
@@ -5,8 +5,28 @@ const hamburgerButton = document.querySelector('.hamburger-button');
 const sidebar = document.querySelector('.sidebar');
 
 // Agregar un evento de clic al botón hamburguesa
-hamburgerButton.addEventListener('click', () => {
-  sidebar.classList.toggle('sidebar--hidden');
-});
+    sidebar.classList.add('sidebar--hidden');
 
-  });
+    // Agregar un evento de clic al botón hamburguesa
+    hamburgerButton.addEventListener('click', () => {
+        sidebar.classList.toggle('sidebar--hidden');
+    });
+});
+function mostrarMenu() {
+    var body = document.querySelector('body');
+    body.classList.add('sidebar-visible');
+}
+function ocultarMenu() {
+    var body = document.querySelector('body');
+    body.classList.remove('sidebar-visible');
+}
+var hamburgerB = document.querySelector('#boton-nav');
+hamburgerB.addEventListener('click', function () {
+    var body = document.querySelector('body');
+    if (body.classList.contains('sidebar-visible')) {
+        ocultarMenu();
+    } else {
+
+        mostrarMenu();
+    }
+});
