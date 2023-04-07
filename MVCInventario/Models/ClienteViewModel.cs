f715c95d6c668e7dcd96ec029e09e09f1f7651d5
ari8191 <128554238+ari8191@users.noreply.github.com>
2023-04-07 14:07:22 -0500

    Add files via upload

---
+++ MVCInventario/Models/ClienteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCInventario.Models
+{
+    public class ClienteViewModel
+    {
+        //Lista de clientes
+        public List<CLIENTE> Clientes { get; set; }
+        public string CedString { get; set; }
+    }
+}
