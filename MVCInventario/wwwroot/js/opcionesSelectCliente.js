fac8da4d77cea64606f8fbaa8b10ebda819b0842
Daniel Bonilla <danilo-parra258@hotmail.com>
2023-04-08 19:50:26 -0500

    Vista cliente v2

---
+++ MVCInventario/wwwroot/js/opcionesSelectCliente.js
@@ -0,0 +1,13 @@
+﻿var select = document.getElementById("selectCliente");
+var div = document.getElementById("opcionCliente");
+var btnBuscar = document.getElementById("btnBuscar");
+select.addEventListener("change", () => {
+    btnBuscar.value = "Buscar"
+    if (select.value === "1") {
+        div.innerHTML = '<input name="CedString" type="text" asp-for="CedString" id="search-input" placeholder="Ingrese la cédula">';
+    } else if (select.value === "2") {
+        div.innerHTML = '<input name="NomString" type="text" asp-for="NomString" id="search-input" placeholder="Ingrese el nombre">';
+    } else {
+        div.innerHTML = "<h1>¿Eh?</h1>";
+    }
+});
